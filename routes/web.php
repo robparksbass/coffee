f086a84aedae0ef6ffcc0d1e1ae2df8e94e7b81a
Rob Parks <roberteparks@students.abtech.edu>
2017-10-31 14:36:06 -0400

    end of tutorial 5

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ Route::get('/', [
     'as' => 'product.index'
 ]);
 
+Route:: get('signup', [
+    'uses' => 'UserController@getSignup',
+    'as' => 'user.signup'
+]);
+
+Route::post('/signup', [
+    'uses' => 'UserController@postSignup',
+    'as' => 'user.signup'   
+]);
