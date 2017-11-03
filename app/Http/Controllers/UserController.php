381222db82a01209d2a46327d1cbb37372a8b40d
Rob Parks <roberteparks@students.abtech.edu>
2017-11-03 10:56:58 -0400

    end of tutorial 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
+
+use Auth;
+
 class UserController extends Controller
 {
     public function getSignup() {
@@ -25,4 +28,25 @@ class UserController extends Controller
 
         return redirect()->route('product.index');
     }
+
+    public function getSignin() {
+        return view('user.signin');
+    }
+
+    public function postSignin(Request $request) {
+        $this->validate($request, [
+            'email' => 'email|required',
+            'password' => 'required|min:4'
+        ]);
+    
+    if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            return redirect()->route('user.profile');
+        }
+        return redirect()->back();
+    }
+    public function getProfile() {
+        return view('user.profile');
+    }
 }
+
+
