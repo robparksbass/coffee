886bf0a6cf4e745c89786bf76798bcc81a59bd3c
Rob Parks <roberteparks@students.abtech.edu>
2017-11-09 15:07:33 -0500

    middle of 8 with issues

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ Route::get('/', [
     'as' => 'product.index'
 ]);
 
+Route::get('add-to-cart/{id}', [
+    'uses' => 'ProductController@getAddToCart',
+    'as' => 'product.addToCart'
+]);
+
 
 Route::group(['middleware' => 'guest'], function(){
 
