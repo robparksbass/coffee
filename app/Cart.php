ef89baad235e7974f6bc6c333a90d918eb26620a
Rob Parks <roberteparks@students.abtech.edu>
2017-11-14 13:00:19 -0500

    end of tutorial 8

--- app/Cart.php
+++ app/Cart.php
@@ -22,7 +22,7 @@ class Cart
     public function add($item, $id) {
         $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
         if ($this->items) {
-            if (array_key_exists($id, $this->$items)) {
+            if (array_key_exists($id, $this->items)) {
                 $storedItem = $this->items[$id];
             }
         }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,60 +18,15 @@ class ProductController extends Controller
             'products' => $products
         ]);        
     }
-//tutorial creator's:
+
     public function getAddToCart(Request $request, $id)
     {
         $product = Product::find($id);
         $oldCart = Session::has('cart') ? Session::get('cart') : null;
         $cart = new Cart($oldCart);
         $cart->add($product, $product->id);
+
         $request->session()->put('cart', $cart);
         return redirect()->route('product.index');
     }
 }
-
-//mine:
-    // public function getAddToCart(Request $request, $id) {
-    //     $product = Product::find($id);
-    //     $oldCart = Session::has('cart') ? Session::get('cart') : null;
-    //     $cart = new Cart($oldCart);
-    //     $cart->add($product, $product->id);
-        
-    //     $request->session()->put('cart', $cart);
-    //     dd($request->session()->get('cart'));
-        
-    //     return redirect()->route('product.index');
-        
-    //     if(!$oldCart) { 
-    //      $cart = new Cart($oldCart); 
-    //     } 
-    //     // dd($cart);
-    //     $cart->add($product, $product->id);
-        
-        
-//         //  $product = $product->id; 
-//         //  $request->session()->put('cart', $cart);
-//         // $cart->add($product, $product->id);
-
-//         $request->session()->put('cart', $cart);
-//         dd($request->session()->get('cart'));
-//         return redirect()->route('product.index');
-//     }
-// }
-
-//Sean's:
-
-//     public function getAddToCart(Request $request, $id){ 
-//          $product = Product::find($id); 
-//          $cart = Session::has('cart') ? Session::get('cart') : null;
-//          //$cart = new Cart($oldCart); 
-//          if(!$cart) {
-//          $cart = new Cart($cart); 
-//          } 
-//          //$product = $product->id; 
-//          $cart->add($product, $product->id); 
-//          Session::put('cart', $cart); 
-//          //$request->session()->put('cart', $cart);
-//          //var_dump($request->session()->get('cart')); 
-//          return redirect()->route('product.index'); }
-// }
