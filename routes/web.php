af6778f2e4b68ef58dd6e9354555f84ca45c1582
Rob Parks <roberteparks@students.abtech.edu>
2017-11-21 13:37:45 -0500

    end of 10

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,12 @@ Route::get('shopping-cart', [
 Route::get('/checkout', [
     'uses' => 'ProductController@getCheckout',
     'as' => 'checkout'
-])
+]);
+
+Route::post('/checkout', [
+    'uses' => 'ProductController@postCheckout',
+    'as' => 'checkout'
+]);
 
 
 Route::group(['middleware' => 'guest'], function(){
